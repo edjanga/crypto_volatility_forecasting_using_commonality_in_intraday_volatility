2cbc525c80932a2da948a5a75a5bccdfd9031382
Emmanuel Djanga <emmanuel.djanga@live.be>
2023-06-04 11:38:19 +0100

    Changes in *_read methods.

--- data_centre/data.py
+++ data_centre/data.py
@@ -14,22 +14,34 @@ class Reader:
     def __init__(self, file: typing.Union[typing.List[str], str] = os.path.abspath('./data_centre/tmp/aggregate2022')):
         self._directory = file
 
-    def returns_read(self, cutoff_low: float = .01, cutoff_high: float = .01, raw: bool=False) -> pd.DataFrame:
+
+    def returns_read(self, cutoff_low: float = .01, cutoff_high: float = .01, raw: bool=False,
+                     resampled: bool=True, symbol: typing.Union[typing.List[str], str]=None) -> pd.DataFrame:
         returns_df = pd.read_parquet(os.path.abspath(self._directory), columns=['timestamp', 'pret_1m', 'symbol'])
         returns_df = pd.pivot(index='timestamp', columns='symbol', values='pret_1m', data=returns_df)
+        if symbol:
+            if isinstance(symbol, str):
+                symbol = [symbol]
+            returns_df = returns_df[symbol]
+        else:
+            returns_df.drop('BUSDUSDT', axis=1, inplace=True)
         returns_df.index = pd.to_datetime(returns_df.index)
         returns_df.dropna(inplace=True)
-        returns_df.drop('BUSDUSDT', axis=1, inplace=True)
         """Winsorise returns"""
         if not raw:
             returns_df = returns_df.apply(lambda x: winsorize(x, (cutoff_low, cutoff_high)))
-        returns_df = returns_df.resample('5T').sum()
+        if resampled:
+            returns_df = returns_df.resample('5T').sum()
         returns_df.replace(0, np.nan, inplace=True)
         returns_df.ffill(inplace=True)
         return returns_df
 
-    def rv_read(self, cutoff_low: float = .01, cutoff_high: float = .01, raw: bool=False) -> pd.DataFrame:
-        rv_df = self.returns_read(cutoff_low=cutoff_low, cutoff_high=cutoff_high, raw=raw)**2
+    def rv_read(self, cutoff_low: float = .01, cutoff_high: float = .01, raw: bool=False,
+                symbol: typing.Union[typing.List[str],str]=None)\
+            -> pd.DataFrame:
+        rv_df = \
+            self.returns_read(cutoff_low=cutoff_low, cutoff_high=cutoff_high,
+                              raw=raw, resampled=False, symbol=symbol)**2
         rv_df = rv_df.resample('5T').sum()
         return rv_df
 
@@ -62,3 +74,15 @@ class Reader:
         csr_df = pd.DataFrame(data=Reader._min_max_scaler.fit_transform(X=csr_df),
                               index=csr_df.index, columns=csr_df.columns)
         return csr_df
+
+    def correlation_read(self, cutoff_low: float = .01, cutoff_high: float = .01, raw: bool=False) -> pd.DataFrame:
+        returns = self.returns_read(cutoff_low=cutoff_low, cutoff_high=cutoff_high, raw=raw)
+        correlation_matrix = returns.rolling(window=4).corr().dropna()
+        correlation = correlation_matrix.droplevel(axis=0, level=1).mean(axis=1)
+        correlation = correlation.groupby(by=correlation.index).mean()
+        correlation.replace(np.inf, np.nan, inplace=True)
+        correlation.replace(-np.inf, np.nan, inplace=True)
+        correlation.ffill(inplace=True)
+        correlation.name = 'CORR'
+        correlation = pd.DataFrame(correlation)
+        return correlation
