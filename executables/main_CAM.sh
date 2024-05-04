44ad98ce161900ce6077e65f07e5e2504f5cb9e3
Emmanuel Djanga <emmanuel.djanga@live.be>
2024-05-04 13:18:51 +0100

    Uncomment comments.

--- executables/main_CAM.sh
+++ executables/main_CAM.sh
@@ -4,22 +4,22 @@ source ../venv/bin/activate
 #######################################################
 ## Computations for CAM.
 #######################################################
-model=har_eq
-for l in {1M,6M}
+for l in {1W,1M,6M}
   do
     for regression_type in {lightgbm,elastic,lasso,ridge,pcr}
       do
-        # for model in {har,ar}
-           # do
-         if [ $model == "har_eq" ]; then
-           # python3 ../generate_results.py --L=$l --model=$model --training_scheme=CAM \
-           # --regression=$regression_type --trading_session=1
-           python3 ../generate_results.py --L=$l --model=$model --training_scheme=CAM \
-           --regression=$regression_type --trading_session=0 --top_book=1
-         else
-           python3 ../generate_results.py --L=$l --model=$model --training_scheme=CAM --regression=$regression_type
-         fi
-           # done
+         for model in {har,ar}
+            do
+               if [ $model == "har_eq" ]; then
+                  python3 ../generate_results.py --L=$l --model=$model --training_scheme=CAM \
+                  --regression=$regression_type --trading_session=1
+                  python3 ../generate_results.py --L=$l --model=$model --training_scheme=CAM \
+                  --regression=$regression_type --trading_session=0 --top_book=1
+               else
+                 python3 ../generate_results.py --L=$l --model=$model --training_scheme=CAM \
+                 --regression=$regression_type
+               fi
+            done
       done
   done
 
--- executables/main_ClustAM.sh
+++ executables/main_ClustAM.sh
@@ -4,22 +4,21 @@ source ../venv/bin/activate
 #######################################################
 ## Computations for ClustAM.
 #######################################################
-model=har_eq
 for l in {1W,1M,6M}
   do
     for regression_type in {lightgbm,elastic,lasso,ridge,pcr}
       do
-        # for model in {ar,har_eq,har}
-          # do
-        if [ $model == "har_eq" ]; then
-          # python3 ../generate_results.py --L=$l --model=$model --training_scheme=ClustAM \
-          # --regression=$regression_type --trading_session=1
-          python3 ../generate_results.py --L=$l --model=$model --training_scheme=ClustAM \
-          --regression=$regression_type --trading_session=0 --top_book=1
-        else
-          python3 ../generate_results.py --L=$l --model=$model --training_scheme=ClustAM \
-          --regression=$regression_type
-        fi
-          # done
+         for model in {ar,har_eq,har}
+           do
+              if [ $model == "har_eq" ]; then
+                 python3 ../generate_results.py --L=$l --model=$model --training_scheme=ClustAM \
+                 --regression=$regression_type --trading_session=1
+                 python3 ../generate_results.py --L=$l --model=$model --training_scheme=ClustAM \
+                 --regression=$regression_type --trading_session=0 --top_book=1
+              else
+                python3 ../generate_results.py --L=$l --model=$model --training_scheme=ClustAM \
+                --regression=$regression_type
+              fi
+           done
       done
   done
