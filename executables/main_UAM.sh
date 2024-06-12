3f616401c56b079f9fb1bfca5d55a3208be2d7cf
Emmanuel Djanga <emmanuel.djanga@live.be>
2024-06-12 10:52:02 +0100

    Add UAM bash script

---
+++ executables/main_UAM.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+clear
+source ../venv/bin/activate
+#######################################################
+## Computations for UAM.
+#######################################################
+for l in {1M,6M}
+  do
+    # python3 ../generate_results.py --L=$l --training_scheme=UAM --regression=var
+    for regression_type in {lstm,lightgbm}
+      do
+        for model in {ar,har,har_eq}
+          do
+            if [ $model == "har_eq" ]; then
+              python3 ../generate_results.py --L=$l --model=$model --training_scheme=UAM --regression=$regression_type \
+              --trading_session=0 --top_book=1
+              python3 ../generate_results.py --L=$l --model=$model --training_scheme=UAM --regression=$regression_type \
+              --trading_session=1
+            else
+              python3 ../generate_results.py --L=$l --model=$model --training_scheme=UAM --regression=$regression_type
+            fi
+          done
+      done
+  done
